da2bd4c190872fcb0997ca2588759bf6cf8fe797
Kolya Toporkov <kolya.toporkov@ddgcorp.ru>
2017-12-08 19:29:57 +0300

    add burn test

--- test/myWishToken.js
+++ test/myWishToken.js
@@ -68,58 +68,17 @@ contract('Crowdsale', accounts => {
         assert.fail(true, false, 'Transaction must be failed because minting is finished.');
     });
 
-    // it('#3 check not yet started', async () => {
-    //     const crowdsale = await Crowdsale.new(TOMORROW, DAY_AFTER_TOMORROW, SOFT_CAP_TOKENS, HARD_CAP_TOKENS);
-    //     (await crowdsale.hasStarted()).should.be.equals(false);
-    // });
-    //
-    // it('#4 check already finished', async () => {
-    //     const crowdsale = await Crowdsale.new(NOW + 30, TOMORROW, SOFT_CAP_TOKENS, HARD_CAP_TOKENS);
-    //     await increaseTime(2 * DAY);
-    //     (await crowdsale.hasStarted()).should.be.equals(true);
-    //     (await crowdsale.hasEnded()).should.be.equals(true);
-    // });
+    it('#4 burn', async () => {
+        const token = await Token.deployed();
 
-    // it('#5 check simple buy token', async () => {
-    //     const crowdsale = await Crowdsale.new(NOW + 30, TOMORROW, SOFT_CAP_TOKENS, HARD_CAP_TOKENS);
-    //     await increaseTime(50);
-    //     const ETH = web3.toWei(1, 'ether');
-    //     const TOKENS = ETH * RATE;
-    //     console.log("START");
-    //     await crowdsale.sendTransaction({from: BUYER_1, value: ETH});
-    //     console.log("END");
-    //     const token = Token.at(await crowdsale.token());
-    //     (await token.balanceOf(BUYER_1)).toNumber().should.be.equals(TOKENS);
-    //     (await new Promise(function (resolve, reject) {
-    //         web3.eth.getBalance(COLD_WALLET, function (error, result) {
-    //             if (error) {
-    //                 reject(error);
-    //             } else {
-    //                 resolve(result);
-    //             }
-    //         })
-    //     })).toNumber().should.be.equals(Number(ETH), 'money should be on cold wallet');
-    // });
-
-    // it('#7 check hard cap', async() => {
-    //     // const hardCap = PRE_SOLD_TOKENS + 1000000;
-    //     const crowdsale = await Crowdsale.new(YESTERDAY, TOMORROW, SOFT_CAP_TOKENS, HARD_CAP_TOKENS);
-    //
-    //     const eth = web3.toWei(500, 'ether'); // > 1000000/1950;
-    //     await crowdsale.sendTransaction({from: REACH_MAN, value: eth});
-    //
-    //     const moreOne = web3.toWei(1, 'ether');
-    //     try {
-    //         await crowdsale.sendTransaction({from: BUYER_1, value: moreOne});
-    //     }
-    //     catch (error) {
-    //         error.message.search('invalid opcode').should.be.greaterThan(0, 'error should be "invalid opcode"');
-    //         // move hard cap forward
-    //         await crowdsale.setHardCap(hardCap + 1000000);
-    //         // buy one more
-    //         await crowdsale.sendTransaction({from: BUYER_1, value: moreOne});
-    //         return;
-    //     }
-    //     assert.fail(true, false, 'Transaction must be failed because of hardcap.');
-    // });
+        const tokensToMint = web3.toWei(1, 'ether');
+        await token.mint(OWNER, tokensToMint);
+        try {
+            tokensToMint.burn(tokensToMint + 1);
+        } catch (error) {
+            token.burn(tokensToMint / 2);
+            return;
+        }
+        assert.fail(true, false, 'Transaction must be failed because it tries to burn more tokens than there is.');
+    });
 });
