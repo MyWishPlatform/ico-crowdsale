5db51a8831e21b1fab723ef129448fc328a11af9
Kolya Toporkov <kolya.toporkov@ddgcorp.ru>
2017-12-13 17:01:39 +0300

    add ganache timefix. fixed contract

--- template/TemplateCrowdsale.sol
+++ template/TemplateCrowdsale.sol
@@ -51,7 +51,7 @@ contract TemplateCrowdsale is usingConsts, MainCrowdsale
      * @dev override token creation to set token address in constructor.
      */
     function createTokenContract() internal returns (MintableToken) {
-        return 0;
+        return MintableToken(0);
     }
 
     //#if "D_AUTO_FINALISE" != "false"
