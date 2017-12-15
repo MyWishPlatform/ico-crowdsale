8187a4d4a82766bf21c7044c29a33730ea1307c6
Valery Dubrava <valery.dubrava@gmail.com>
2017-12-15 15:42:11 +0300

    run all tests; remove unnecessary test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 node node_modules/.bin/c-preprocessor --config c-preprocessor-config.json template/test/templateCrowdsale.js test/templateCrowdsale.js
-node_modules/.bin/truffle test ./test/templateCrowdsale.js
+node_modules/.bin/truffle test
