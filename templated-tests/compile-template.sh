783c117fe2439c496f318af4cbbe6926c3a368ac
kolya-t <toporkov.kolya96@gmail.com>
2017-12-24 16:45:09 +0300

    add 'set -e' to scripts begin to abort compiling when preprocessing failed

--- templated-tests/compile-template.sh
+++ templated-tests/compile-template.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 rm -rf build
 rm -f contracts/TemplateCrowdsale.sol contracts/BonusableCrowdsale.sol test/templateCrowdsale.js
 source templated-tests/preprocess-template.sh $1
