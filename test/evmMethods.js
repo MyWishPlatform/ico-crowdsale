ae329834d586c73172a18665332ccf2c339210d5
Valery Dubrava <valery.dubrava@gmail.com>
2018-02-20 17:40:52 +0300

    add timeTo method

--- test/evmMethods.js
+++ test/evmMethods.js
@@ -14,6 +14,33 @@ const mineBlock = () => {
 };
 
 module.exports = {
+    timeTo: absoluteTime => {
+        return new Promise((resolve, reject) => {
+            return web3.eth.getBlockNumber((e, blockNumber) => {
+                return web3.eth.getBlock(blockNumber, (e, block) => {
+                    const blockTs = block.timestamp;
+                    if (blockTs >= absoluteTime) {
+                        if (blockTs > absoluteTime) {
+                            console.warn("Block time already is in future:", blockTs, ">", absoluteTime)
+                        }
+                        resolve(true);
+                    }
+                    const delta = absoluteTime - blockTs;
+                    return web3.currentProvider.sendAsync(
+                        [{jsonrpc: "2.0", method: "evm_increaseTime", params: [delta], id: 0},
+                            {jsonrpc: "2.0", method: "evm_mine", params: [], id: 0}],
+                        function (error, result) {
+                            if (error) {
+                                reject(error);
+                            } else {
+                                resolve(result);
+                            }
+                        }
+                    );
+                });
+            });
+        });
+    },
     increaseTime: addSeconds => {
         return new Promise((resolve, reject) => {
             web3.currentProvider.sendAsync(
