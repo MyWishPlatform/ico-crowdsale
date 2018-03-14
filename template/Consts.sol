79bc0b8852eb231b1ebae8ec05374da797640f83
kolya-t <toporkov.kolya96@gmail.com>
2018-03-14 19:25:31 +0300

    templated Consts.sol for START_TIME constant (it used only for crowdsale contract)

--- template/Consts.sol
+++ template/Consts.sol
@@ -9,6 +9,8 @@ contract usingConsts {
     string constant TOKEN_SYMBOL = "D_SYMBOL";
     bool constant PAUSED = D_PAUSE_TOKENS;
     address constant TARGET_USER = D_CONTRACTS_OWNER;
+    //#if !defined(D_ONLY_TOKEN) || D_ONLY_TOKEN != true
     uint constant START_TIME = D_START_TIME;
+    //#endif
     bool constant CONTINUE_MINTING = D_CONTINUE_MINTING;
 }
--- templated-tests/preprocess-crowdsale-template.sh
+++ templated-tests/preprocess-crowdsale-template.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p build/template
 echo "first pass"
+node node_modules/.bin/c-preprocessor --config pre-firstpass-config.json template/Consts.sol build/template/Consts.sol
 node node_modules/.bin/c-preprocessor --config pre-firstpass-config.json template/TemplateCrowdsale.sol build/template/TemplateCrowdsale.sol
 node node_modules/.bin/c-preprocessor --config pre-firstpass-config.json template/BonusableCrowdsale.sol build/template/BonusableCrowdsale.sol
 node node_modules/.bin/c-preprocessor --config pre-firstpass-config.json template/MainToken.sol build/template/MainToken.sol
@@ -8,6 +9,6 @@ node node_modules/.bin/c-preprocessor --config pre-firstpass-config.json templat
 echo "second pass"
 node node_modules/.bin/c-preprocessor --config $1 build/template/TemplateCrowdsale.sol contracts/TemplateCrowdsale.sol
 node node_modules/.bin/c-preprocessor --config $1 build/template/MainToken.sol contracts/MainToken.sol
-node node_modules/.bin/c-preprocessor --config $1 template/Consts.sol contracts/Consts.sol
+node node_modules/.bin/c-preprocessor --config $1 build/template/Consts.sol contracts/Consts.sol
 node node_modules/.bin/c-preprocessor --config $1 build/template/BonusableCrowdsale.sol contracts/BonusableCrowdsale.sol
 node node_modules/.bin/c-preprocessor --config $1 build/template/2_deploy_contracts.js migrations/2_deploy_contracts.js
