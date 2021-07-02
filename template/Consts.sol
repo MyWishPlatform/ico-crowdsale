8dfc598fbb7a9670afe208d41e453bc15c2b0412
nenyk <2d1n6a@gmail.com>
2021-07-02 05:05:23 +0300

    0.8.4 update 3/2

--- template/Consts.sol
+++ template/Consts.sol
@@ -9,7 +9,6 @@ contract Consts {
 
     string public constant TOKEN_NAME = "D_NAME";
     string public constant TOKEN_SYMBOL = "D_SYMBOL";
-    bool public constant PAUSED = D_PAUSE_TOKENS;
     address public constant TARGET_USER = D_CONTRACTS_OWNER;
     //#if !defined(D_ONLY_TOKEN) || D_ONLY_TOKEN != true
     uint public constant START_TIME = D_START_TIME;
